cbd0255b1ece3018ce672d8a01b34ea3b00106b1
Saketku98 <88224770+Saketku98@users.noreply.github.com>
2023-08-22 19:28:23 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="card footer">
+      <h2>GitFind | Developed by Saket &amp; Team | &copy; 2023</h2>
+      <p> Made with ❤️ &amp; 💻</p>
+    </div>
+  )
+}
+
+export default Footer;
